ee588b568f0ea0aeb9b1114351facebf06de6aeb
Chris Tarasovs <christarasovs@chriss-imac.home>
2017-03-19 20:52:24 +0000

    added pipe

--- src/app/pipes/menu-type.pipe.ts
+++ src/app/pipes/menu-type.pipe.ts
@@ -3,15 +3,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({
   name: 'menuType'
 })
+
 export class MenuTypePipe implements PipeTransform {
+ private ids: number[] = [0, 1, 11];
 
-// id 0, 1, 11
-
-   // transform(items: Array<int>, id: int): Array<int> {
-     //   return items.find(id);
-//    }
-
-
-
-
+    transform(items: any[]): any {
+     return items.filter((key: any) => {
+      return this.ids.includes(key.id);
+        });
+    }
 }
+
