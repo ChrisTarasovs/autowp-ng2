105c4232a72ca973759904cf66ff2da1df85430c
Kristaps Tarasovs <kristaps.tarasovs@gmail.com>
2017-06-15 16:41:44 +0100

    Add files via upload

--- src/app/pipes/safeurl.pipe.ts
+++ src/app/pipes/safeurl.pipe.ts
@@ -2,15 +2,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl, SafeUrl} from '@angular/platform-browser';
 
 
-
-@Pipe({name: 'safeHtml'})
-export class SafeHtml {
+@Pipe({name: 'safelink'})
+export class Safelink {
   constructor(private sanitized: DomSanitizer){}
 
   transform(value) {
   	console.log(this.sanitized.bypassSecurityTrustUrl(value))
-    return this.sanitized.bypassSecurityTrustHtml(value);
-    //return this.sanitized.bypassSecurityTrustUrl(value);
+    //return this.sanitized.bypassSecurityTrustHtml(value);
+     return this.sanitized.bypassSecurityTrustUrl(value);
     //return this.sanitizer.bypassSecurityTrustStyle(style);
     // return this.sanitizer.bypassSecurityTrustXxx(style); - see docs
   }
